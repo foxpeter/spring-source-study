35ae57904a5a117484b149dc4c581c2f8a282403
meihanwen <meihanwen@jd.com>
2016-09-06 19:58:28 +0800

    update

---
+++ src/test/java/SpringStart.java
@@ -0,0 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by mhw on 2016/9/6.
+ */
+public class SpringStart {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        context.getBean("test");
+    }
+}
